7d62d328add0c1ba57fe7129ad6dcb40570de55a
alex beltrannn <alexbeltrn@outlook.com>
2026-01-04 14:20:51 -0600

    Implement NotesStore singleton and note management
    Added a singleton NotesStore class to manage notes, including methods for adding, deleting, saving, and loading notes. Updated ViewController to display notes in a table view and handle note addition. These changes enable persistent note storage and basic note list UI functionality.

--- NotesStore.m
+++ NotesStore.m
@@ -6,8 +6,70 @@
 //
 
 #import "NotesStore.h"
+#include <objc/objc.h>
+#include <Foundation/Foundation.h>
+#include <dispatch/dispatch.h>
 #import "Note.h"
 
 @interface NotesStore ()
 @property (nonatomic, strong) NSMutableArray<Note *> *privateNotes;
+@end
+
+@implementation NotesStore
+
++ (instancetype)sharedStore {
+    static NotesStore *store;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        store = [[NotesStore alloc] initPrivate];
+    });
+    return store;
+}
+
+- (instancetype)initPrivate {
+    self = [super init];
+    if (self) {
+        _privateNotes = [NSMutableArray array];
+    }
+    return self;
+}
+
+- (NSArray<Note *> *)allNotes {
+    return [_privateNotes copy];
+}
+
+- (void)addNote:(Note *) note {
+    [self.privateNotes addObject:note];
+    [self save];
+}
+
+- (void)deleteNote:(Note *)note {
+    [self.privateNotes removeObject:note];
+    [self save];
+}
+
+- (NSString *)notesPath {
+    NSString *dir =
+      NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
+                                          NSUserDomainMask, YES).firstObject;
+    return [dir stringByAppendingPathComponent:@"notes.plist"];
+}
+
+- (void)save {
+    NSMutableArray *array = [NSMutableArray array];
+    for (Note *note in self.privateNotes) {
+        [array addObject:[note dictionaryRepresentation]];
+    }
+    [array writeToFile:[self notesPath] atomically:YES];
+}
+
+- (void)load {
+    NSArray *saved =
+      [NSArray arrayWithContentsOfFile:[self notesPath]];
+    for (NSDictionary *dict in saved) {
+        [self.privateNotes addObject:
+         [Note noteFromDictionary:dict]];
+    }
+}
+
 @end
--- ViewController.m
+++ ViewController.m
@@ -5,18 +5,54 @@
 //  Created by Alejandro Beltrán on 1/2/26.
 //
 
-#import "ViewController.h"
-
-@interface ViewController ()
-
-@end
+#import "NotesListViewController.h"
+#import "NotesStore.h"
+#import "Note.h"
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    self.title = @"Notes";
+    self.navigationItem.rightBarButtonItem =
+      [[UIBarButtonItem alloc] initWithBarButtonSystemItem:
+       UIBarButtonSystemItemAdd
+       target:self
+       action:@selector(addNote)];
 }
 
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section {
+    return [[[NotesStore sharedStore] allNotes] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    UITableViewCell *cell =
+      [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+
+    if (!cell) {
+        cell = [[UITableViewCell alloc]
+                initWithStyle:UITableViewCellStyleSubtitle
+                reuseIdentifier:@"Cell"];
+    }
+
+    Note *note =
+      [[NotesStore sharedStore] allNotes][indexPath.row];
+
+    cell.textLabel.text = note.title;
+    cell.detailTextLabel.text = note.content;
+
+    return cell;
+}
+
+- (void)addNote {
+    Note *note = [[Note alloc] initWithTitle:@"New Note"
+                                     content:@""];
+    [[NotesStore sharedStore] addNote:note];
+    [self.tableView reloadData];
+}
 
 @end
